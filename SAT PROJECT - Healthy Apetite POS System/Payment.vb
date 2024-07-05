82707bd4a630b67df3ad529d11abf964799ea0d8
BEG5210 <91362412+BEG5210@users.noreply.github.com>
2024-07-05 18:59:58 +1000

    Sos about random commits before this, i was having an issue where i couldnt commit. sos me.

--- SAT PROJECT - Healthy Apetite POS System/Payment.vb
+++ SAT PROJECT - Healthy Apetite POS System/Payment.vb
@@ -26,9 +26,9 @@
     End Sub
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles chkManual.CheckedChanged
-        'h
+
         'Check if Manual input is checked
-        If chkManual.Checked = False Then '
+        If chkManual.Checked = False Then
             'User cannot edit Text Boxes
             txtAmountOwed.ReadOnly = True
             txtAmountPaid.ReadOnly = True
