f6a941b3954147fc48ae7306458c91059ff59854
BEG5210 <91362412+BEG5210@users.noreply.github.com>
2024-07-05 18:58:39 +1000

    j

--- SAT PROJECT - Healthy Apetite POS System/Payment.vb
+++ SAT PROJECT - Healthy Apetite POS System/Payment.vb
@@ -28,7 +28,7 @@
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles chkManual.CheckedChanged
         'h
         'Check if Manual input is checked
-        If chkManual.Checked = False Then
+        If chkManual.Checked = False Then '
             'User cannot edit Text Boxes
             txtAmountOwed.ReadOnly = True
             txtAmountPaid.ReadOnly = True
