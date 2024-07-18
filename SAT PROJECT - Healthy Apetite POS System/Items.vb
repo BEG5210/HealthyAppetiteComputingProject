5c4ebd9bb8cf66fdc1b4ac8507b6ca53c9b7469a
BEG5210 <91362412+BEG5210@users.noreply.github.com>
2024-07-18 12:49:07 +1000

    added items class

--- SAT PROJECT - Healthy Apetite POS System/Items.vb
+++ SAT PROJECT - Healthy Apetite POS System/Items.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Items
 
+#Region "What is this class for?"
+    'this class is meant to save the data of the various items for sale, including their:
+    'stock
+    'colour
+    'name
+    'price
+    'and more
+    '
+    'after the csv containing this info is read, it will be saved here.
+    'once the user is ready to exit, then the csv is saved, using the updated data
+    'witch is stored here.
+    'anyway, good luck coding!
+
+    '   - MEEEEEEEEEEEEEE
+#End Region
+
 End Class
