e349651cd798ced2e51127ff33240fd71b594d50
BEG5210 <91362412+BEG5210@users.noreply.github.com>
2024-08-07 13:24:20 +1000

    Each thing is its its index. idk how to explain it

--- SAT PROJECT - Healthy Apetite POS System/Main.vb
+++ SAT PROJECT - Healthy Apetite POS System/Main.vb
@@ -21,29 +21,43 @@ Public Class Main
     End Sub
 
     Private Sub ReadItemCSV()
-        'defining variables
-        Dim FileName As String
+
+        ' Define arrays to store the parsed values
+        Dim nums As New List(Of Integer)()
+        Dim names As New List(Of String)()
+        Dim colours As New List(Of String)()
+        Dim prices As New List(Of Decimal)()
+        Dim stocks As New List(Of Integer)()
+
+        ' Define file variables
+        Dim FileName As String = "test.csv"
+        Dim filepath As String = "C:/Computing/" & FileName
         Dim FileNum As Integer = FreeFile()
         Dim i As Integer = 0
 
-        'identify file directory
-        FileName = "test.csv"
-        filepath = "C:/Computing/" + FileName
-
+        ' Open the file
         FileOpen(FileNum, filepath, OpenMode.Input)
 
         Try
-            'reads each line of the file, and saves each row to a different index in an array
+            ' Read each line of the file
             Do Until EOF(FileNum)
-                output = LineInput(FileNum)
+                Dim output As String = LineInput(FileNum)
+                Dim fields() As String = Split(output, ",")
 
-                items(i) = output
+                ' Check if the line has the correct number of fields
+                If fields.Length = 5 Then
+                    nums.Add(CInt(fields(0)))
+                    names.Add(fields(1) & " " & fields(2))
+                    colours.Add(fields(2))
+                    prices.Add(CDec(fields(3)))
+                    stocks.Add(CInt(fields(4)))
+                End If
 
+                ' Representation of data for testing
+                txtTest.Text += output & vbCrLf
 
-                txtTest.Text += output + vbCrLf 'Representation of Data for testing
-
-                i += 1                          'Iterate index by 1
-
+                ' Iterate index by 1
+                i += 1
             Loop
 
             FileClose(FileNum)
@@ -53,6 +67,14 @@ Public Class Main
             MsgBox("Error Reading Items CSV File.")
         End Try
 
+        ' Print the results for testing
+        txtTest.Text += "Num: {" & String.Join(", ", nums.ToArray()) & "}" & vbCrLf
+        txtTest.Text += "Name: {" & String.Join(", ", names.ToArray()) & "}" & vbCrLf
+        txtTest.Text += "Colour: {" & String.Join(", ", colours.ToArray()) & "}" & vbCrLf
+        txtTest.Text += "Price: {" & String.Join(", ", prices.ToArray()) & "}" & vbCrLf
+        txtTest.Text += "Stock: {" & String.Join(", ", stocks.ToArray()) & "}" & vbCrLf
+
+        ' Reset the index
         i = 0
 
     End Sub
@@ -72,6 +94,11 @@ Public Class Main
         ShiftManager.Show()
     End Sub
 
+    'Testing:
+    Private Sub cmd1_1_Click(sender As Object, e As EventArgs) Handles cmd1_1.Click
+        MsgBox(strVella)
+    End Sub
+
 #Region "------ IMPORTANT INFO -----------------------------------------"
     '
     '******************* ITEM BUTTON NAME FORMATTING ***************
@@ -87,12 +114,18 @@ Public Class Main
 #End Region
 
 #Region "------ NOTES TO SELF ------------------------------------------"
-
+    '
     'MAXIMUM LINES IN CART TEXTBOX BEFORE SCROLLING IS NEEDED IS 17! AS OF RIGHT NOW
-
+    '
     'INCASE OF ANY ERRORS - there is an important annotation in the bottom of
     'application designer that may be usefull
+    '
+#End Region
 
+#Region "--------- CREDITS ---------"
+    '
+    'Portions of "ReadItemCSV()" were written by chatgtp, specifically the portions wwhere data was saved to lists
+    '
 #End Region
 
 End Class
