7c75625bf282d44046871979686f7a146f4de085
BEG5210 <91362412+BEG5210@users.noreply.github.com>
2024-08-16 10:49:53 +1000

    Fixed Annotations in Payment.vb

--- SAT PROJECT - Healthy Apetite POS System/Payment.vb
+++ SAT PROJECT - Healthy Apetite POS System/Payment.vb
@@ -4,15 +4,14 @@
 
 #Region "Defining Variables"
     'DEFINING SOME VARIABLES:
-
-    'denominations:         {5,10,20,50, 1, 2, 5,10,20,50,100}
+    'denominations:            {5,10,20,50, 1, 2, 5,10,20,50,100}
     Dim CashNum As Integer() = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}    '11 values (11-1)
     '                          [   CENTS   ][      DOLLARS      ]
-
+    '---------------------------------------------------------------------------------
     'denominations:               {5,10,20,50, 1, 2, 5,10,20,50,100}
     Dim CashChange As Integer() = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} '11 values (11-1)
     '                             [   CENTS   ][      DOLLARS      ]
-
+    '---------------------------------------------------------------------------------
     'denominations:              {5,10,20,50, 1, 2, 5,10,20,50,100}
     Dim CashTotal As Decimal() = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}  '11 values (11-1)
     '                            [   CENTS   ][      DOLLARS      ]
