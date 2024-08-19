780d40f103b18e3cf89fcdcb35c6c7d5fe8a80cf
BEG5210 <91362412+BEG5210@users.noreply.github.com>
2024-08-19 10:00:39 +1000

    removed code scrap / redundant code

--- SAT PROJECT - Healthy Apetite POS System/Discount.vb
+++ SAT PROJECT - Healthy Apetite POS System/Discount.vb
@@ -1,9 +1,5 @@
 ﻿Public Class Discount
 
-    Private Sub btnExitDiscount_Click(sender As Object, e As EventArgs)
-        Me.Close()
-    End Sub
-
     Private Sub cmdExit_Click(sender As Object, e As EventArgs) Handles cmdExit.Click
         Me.Close()
     End Sub
